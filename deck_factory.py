e8f898f9d4b59bc3ae81d08e238f9be32cf681af
Corbin Johnson <corbinjohnson@me.com>
2016-05-10 19:19:20 -0500

    added a function to create the cards from a given deck format.  Associated test added.

--- deck_factory.py
+++ deck_factory.py
@@ -1,22 +1,52 @@
 from enum import Enum
 import json
+import re
 
 Power = Enum("Power", "Bonus Clone Discard Go Poison Rescue Reverse Skip")
 
 class Card:
 
     def __init__(self, denomination, power, owner):
-        denomination = denomination
-        power = power
-        owner = owner
+        self.denomination = denomination
+        self.power = power
+        self.owner = owner
+
+class Owner:
+
+    def __init__(self, name):
+        name = name
 
 class DeckFactory:
 
-    def __init__(self):
-        deck = []
+    def __init__(self, name):
+        self.deck = []
+        self.owner = Owner(name)
 
+    #TODO: Add a card import verifier to ensure only actual existing tribbles cards are added to a deck.
     def deck_import(self, json_formatted_deck):
         """An example deck might look like this:
-
+            {
+                "cards":
+                    [
+                        "2 100 Tribbles - Rescue",
+                        "1 10 Tribbles - Copy",
+                        "4 100 Tribbles - Copy"
+                    ]
+            }
         """
+
+    def convert_string_to_cards(self, string):
+        """
+        parses cards in the format '<quantity> <denomination> Tribbles - <power>' to one or more Card objects.
+        Returns all cards as a list.
+        """
+        quantity = int(re.findall("(\d+)\s\d+", string)[0])
+        denomination = int(re.findall("\d+\s(\d+)\s", string)[0])
+        power = re.findall("- (.+)$", string)[0]
+
+        to_return = []
+        for i in range(0, quantity):
+            to_return.append(Card(denomination, Power[power], self.owner))
+
+        return to_return
 
--- tests/test_deck_factory.py
+++ tests/test_deck_factory.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import unittest
-from deck_factory import Card, Power
+from deck_factory import Card, Power, DeckFactory
 
 
 class TestDeckFactory(unittest.TestCase):
@@ -11,6 +11,17 @@ class TestDeckFactory(unittest.TestCase):
         p = Power.Poison
         c = Card(1, p, None)
         self.assertTrue(c)
+
+    def test_card_from_string_function(self):
+
+        df = DeckFactory("TestName")
+        card_example = "9 1 Tribble - Go"
+        count = 0
+        for each_card in df.convert_string_to_cards(card_example):
+            count += 1
+            self.assertEqual(each_card.denomination, 1)
+            self.assertEqual(each_card.power, Power.Go)
+        self.assertEqual(count, 9)
 
 if __name__ == '__main__':
     unittest.main()
